8da1caf805cee0f4b853e8be205a4bd6cf175ec5
Ozgur <ozgurkarakas025@gmail.com>
2024-08-04 15:36:35 +0300

    googleAndGithub

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,14 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
- 
-const prisma = new PrismaClient()
- 
+import { Adapter } from "next-auth/adapters";
+import prisma from "./lib/prisma";
+import Google from "next-auth/providers/google"
+import GitHub from "next-auth/providers/github"
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [],
+  theme:{
+    logo:"/heisenberg.png"
+  },
+  adapter: PrismaAdapter(prisma) as Adapter,
+  providers: [Google,GitHub],
 })
