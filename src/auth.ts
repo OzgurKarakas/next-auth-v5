aa5037196a2d12e33f15257a3b5c5dc705f6035f
Ozgur <ozgurkarakas025@gmail.com>
2024-08-07 21:53:34 +0300

    authFix

--- src/auth.ts
+++ src/auth.ts
@@ -1,13 +1,12 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import NextAuth from "next-auth";
+import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import { Adapter } from "next-auth/adapters";
-import GitHub from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
-import Resend from "next-auth/providers/resend";
 import prisma from "./lib/prisma";
+import Google from "next-auth/providers/google"
+import GitHub from "next-auth/providers/github"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  trustHost: true, 
+  trustHost: true,
   theme: {
     logo: "/logo.png",
   },
@@ -20,9 +19,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   },
   providers: [
     Google,
-    GitHub,
-    Resend({
-      from: "no-reply@tutorial.codinginflow.com",
-    }),
-  ],
+    GitHub
+  ]
 });
